773a7c688da26353c7fda6107f3ac808aec2f943
Nile Kelly <nilekell@gmail.com>
2023-11-07 12:00:00 +0000

    lesson 1

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,14 @@
+let js = "amazing";
+
+let firstName = "Jonas";
+console.log(firstName);
+
+let country = "England";
+let continent = "Europe";
+let population = 60000000;
+let statement = `I am from ${country}, which is located in ${continent}. ${country} has a population of ${population}`;
+console.log(statement);
+country = "Jamaica";
+continent = "North America";
+population = 3000000;
+console.log(statement);
